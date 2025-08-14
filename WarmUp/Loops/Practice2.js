cf6f2f510385fcb1c1e0d87bcfe69785c40039f8
Aditya Gupta <adityaguptamorena@gmail.com>
2025-08-14 13:36:34 +0530

    Added some more program

--- WarmUp/Loops/Practice2.js
+++ WarmUp/Loops/Practice2.js
@@ -111,4 +111,24 @@ solution:  function isPalindrome(x) {
             let num = 12321;
             console.log(isPalindrome(num));
 
+8.Reverse Integer with Overflow Check
+Write a function reverse(x) that takes a 32-bit signed integer and returns its digits reversed. If the reversed value overflows the 32-bit signed integer range, return 0.
+
+solution : function reverseInteger(x) {
+
+        let xCopy = x;
+            x = Math.abs(x);
+            let rev = 0;
+            while (x > 0) {
+                let last = x % 10;
+                rev = rev * 10 + last;
+                x = Math.floor(x / 10);
+            }
+            if (rev > 2**31 - 1) return 0;
+            return xCopy < 0 ? -rev : rev;
+        }
+            let num = 12345;
+            console.log(reverseInteger(num));
+
 */
+    
