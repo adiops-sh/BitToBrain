c5e6c2e8ddad533ff0be2387f3a77deab85b46d8
Aditya Gupta <adityaguptamorena@gmail.com>
2025-08-20 21:24:19 +0530

    Added one more Solution

---
+++ Array/Ques_6.js
@@ -0,0 +1,23 @@
+/* Given an integer array nums, move all 0’s to the end of it while maintaining the relative order of the non-zero elements.
+Note: You must do this in-place without making a copy of the array.
+
+solution: 
+
+*/
+    function moveZeros(nums){
+        let x=0;
+        for(let i=0;i<nums.length;i++){
+            if(nums[i] !== 0){ 
+                nums[x] = nums[i];
+                x++;
+            }
+        }
+        for( let i=x;i<nums.length;i++){
+            nums[i] = 0;
+        }
+
+    }
+
+    let nums = [0,1,0,3,12];
+    moveZeros(nums);
+    console.log(nums);
