f62f00f9278230d3cc4868c9fa0880875171c453
Aditya Gupta <adityaguptamorena@gmail.com>
2025-08-22 23:16:23 +0530

    Added few more Program

---
+++ Recursion/Ques_1.js
@@ -0,0 +1,13 @@
+/*
+    Print numbers from n to 1 using recursion
+solution:
+    function print(n){
+        if(n===0){
+            return;
+        }
+        console.log(n);
+        return print(n-1);
+    }
+    print(5);
+
+*/
