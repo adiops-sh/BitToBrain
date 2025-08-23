afb09df73fc2e5ec6994fbc3e312c069b501c4f9
Aditya Gupta <adityaguptamorena@gmail.com>
2025-08-23 20:46:50 +0530

    Added some program

---
+++ Recursion/Ques_4.js
@@ -0,0 +1,10 @@
+/*
+    Function to calculate factorial of a number using Recursion
+    solution: function fact(n){
+            if(n==0 || n==1){
+                return 1;
+            }
+                return n * fact(n-1);
+            }
+                console.log(fact(5));
+*/
