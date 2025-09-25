e8b359f35ab24f11ebba8e00c0df425028e3eba0
Aditya Gupta <adityaguptamorena@gmail.com>
2025-09-25 23:15:26 +0530

    Added insertion sort

--- Sorting/Lecture5.js
+++ Sorting/Lecture5.js
@@ -1,6 +1,12 @@
 function insertionSort(arr){
     let n = arr.length;
+    for(let i=0;i<n-1;i++){
+        for(let j=0 ;j<n-1;j++){
+
+        }
+    }
     
 }
+
 let arr = [5,3,-8,4,2];
 console.log(insertionSort(arr));
